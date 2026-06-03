325174cb3c9ef4dec68bdaa060117f5a36c7bce0
Raphael Najem <99417072+romperium@users.noreply.github.com>
2026-06-03 14:37:06 +0300

    Update jarscrpt.js

--- jarscrpt/jarscrpt.js
+++ jarscrpt/jarscrpt.js
@@ -1,18 +1,1 @@
-const textSelect= [
-  'hop on terraria majd',
-  'i said maybe',
-  'check out the repo at github.com/romperium',
-  'yo does this work?',
-];
- 
-function getRandomText() {
-    const randomIndex = Math.floor(Math.random() * textSelect.length);
-    return textSelect[randomIndex];
-  }
- 
-function loadText() {
-  const myDiv = document.getElementById('div_text');
-  myDiv.innerHTML= getRandomText();
-}
- 
-window.onload = loadText;
+
